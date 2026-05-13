cc51d4468821f4390a456f153906cb5bfd20ba97
Carol Velasco <stefanya09111723@gmail.com>
2026-05-12 21:22:05 -0500

    Inicialización proyecto

---
+++ ternurines/src/test/java/com/ternurines/TernurinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ternurines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TernurinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
